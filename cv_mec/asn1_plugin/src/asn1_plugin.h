11a530552281d7f60661e446db8638ddbcf63940
john-wiens <jwiens3141@gmail.com>
2025-12-09 17:29:28 -0700

    Adding in import for TumData to C bindings

--- cv_mec/asn1_plugin/src/asn1_plugin.h
+++ cv_mec/asn1_plugin/src/asn1_plugin.h
@@ -7,6 +7,12 @@
 #include "generated-files/2024/asn_application.h"
 #include "generated-files/2024/asn_internal.h"
 
+
+// The Tum message defines a field called EncryptedTumData which is just an uper encoded String.
+// Since the contents of that string must also be ASN.1 encoded, they must be encoded separately to build the TUM message
+// This import pulls in the additional methods required to encoded / decode the subfields of TumData
+#include "generated-files/2024/TumData.h"
+
 #include "generated-files/2024/MessageFrame.h"
 
 #if _WIN32
